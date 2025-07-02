79c142df346ef99613f0b1bb8587e3adafb28ea6
LindaAmnell <amnell3@hotmail.com>
2025-07-02 11:15:27 +0200

    small changes

--- src/data/EnSweList.ts
+++ src/data/EnSweList.ts
@@ -1,12 +1,12 @@
 export const aboutMeList = [
-  "Hi! I'm a frontend developer with a keen eye for design and a strong focus on creating smooth, user-friendly experiences. I recently completed my frontend development education, where I gained a solid foundation in modern web technologies—especially React, TypeScript, HTML, and CSS.",
+  "Hi! I'm a developer with a keen eye for design and a strong focus on creating smooth, user-friendly experiences. I recently completed my frontend development education, where I gained a solid foundation in modern web technologies—especially React, TypeScript, HTML, and CSS.",
   "I'm someone who enjoys solving problems and constantly learning new things. I care about the details, whether it's polishing a component or ensuring the user flow feels smooth.",
   "In my free time, I enjoy spending time with my daughter, having fun, or going on little adventures together. When I'm not doing that, I like running, training my dog, or meeting up with friends. To unwind, I follow my favorite Formula 1 team McLaren or work on puzzles.",
   "I believe creativity and structure go hand in hand, and that’s something I bring with me in both work and leisure.",
 ];
 
 export const aboutMeListSv = [
-  "Hej! Jag är en frontendutvecklare med öga för design och ett starkt fokus på att skapa smidiga och användarvänliga upplevelser. Jag har nyligen avslutat min utbildning inom frontendutveckling där jag fick en stabil grund i moderna webbteknologier – särskilt React, TypeScript, HTML och CSS.",
+  "Hej! Jag är en utvecklare med öga för design och ett starkt fokus på att skapa smidiga och användarvänliga upplevelser. Jag har nyligen avslutat min utbildning inom frontendutveckling där jag fick en stabil grund i moderna webbteknologier – särskilt React, TypeScript, HTML och CSS.",
   "Jag är en person som gillar att lösa problem och ständigt lära mig nya saker. Jag bryr mig om detaljer, oavsett om det handlar om att finslipa en komponent eller se till att användarflödet känns smidigt.",
   "På fritiden umgås jag gärna med min dotter och hittar på bus och äventyr tillsammans. När jag inte gör det gillar jag att springa, träna min hund eller träffa vänner. När jag vill koppla av följer jag gärna mitt favoritteam McLaren i Formel 1 eller lägger pussel.",
   "Jag tror att kreativitet och struktur går hand i hand, och det är något jag tar med mig både i arbete och fritid.",
@@ -17,7 +17,8 @@ export const infoText = {
     name: "LINDA AMNELL",
     title: "Frontend Developer",
     description:
-      "I'm a frontend developer who enjoys solving problems and building clear, functional interfaces with a focus on usability and responsiveness.",
+      "I create clear and functional digital experiences with a strong focus on usability and responsive design. Passionate about solving problems and turning ideas into intuitive interfaces.",
+
     resume: "Resume",
     tech: "Technical knowledge",
   },
@@ -25,7 +26,8 @@ export const infoText = {
     name: "LINDA AMNELL",
     title: "Frontendutvecklare",
     description:
-      "Jag är en frontendutvecklare som gillar att lösa problem och bygga tydliga, funktionella gränssnitt med fokus på användarvänlighet och responsivitet.",
+      "Jag skapar tydliga och funktionella gränssnitt med fokus på användarvänlighet och responsiv design. Jag brinner för att lösa problem och förvandla idéer till intuitiva digitala upplevelser.",
+
     resume: "CV",
     tech: "Teknisk kunskap",
   },
